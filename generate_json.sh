a70923dedfbd5785ff5ec3c291bc2e739a9919ca
FantasiososCrisa <pablo.bedialauneta@spacegeneration.org>
2025-09-07 09:25:22 +0200

    Add files via upload

---
+++ generate_json.sh
@@ -0,0 +1,2 @@
+python3 /Users/pablo/Documents/Webapp/fantasyscrapper/scraping_tasks/scrape_laligafantasy.py
+python3 /Users/pablo/Documents/Webapp/reduce_json.py
---
+++ reduce_json.py
@@ -0,0 +1,25 @@
+import json
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+p = "./fantasyscrapper/json_files/laligafantasy_laliga_data.json"
+with open(p, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# Creamos la versión reducida
+reduced_data = [
+    {
+        "name": player["name"],
+        "team": player["team"],
+        "position": player["position"],
+        "price": player["price"],
+        "price_history": player["price_history"],
+        "time_history": player["time_history"]
+    }
+    for player in data
+]
+
+# Guardamos el JSON reducido
+with open('players.json', 'w', encoding='utf-8') as f:
+    json.dump(reduced_data, f, ensure_ascii=False, indent=2)
